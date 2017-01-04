1f0b0adb13e22127366375146709c9a18945ab21
Ulisses Bueno <has.ulisses.silva@jsl.com.br>
2017-01-04 17:12:59 -0200

    #

--- class/tagx.class.php
+++ class/tagx.class.php
@@ -1,4 +1,6 @@
 <?php
+
+
 class tagx{
 
 	var $tree;
@@ -28,7 +30,7 @@ class tagx{
 		$this->tree = $this->tag_tree( $html );
 	}
 
-	var $output = '';
+	var $output_html = '';
 
 	function setValueChave( $str, $data ){
 
@@ -61,7 +63,51 @@ class tagx{
 		eval( " return \$data". $chaves .";" );
 	}
 
-	function write( $data = NULL, $ar = NULL, $breadcrumb = '', $open_parent = '', $parent = ''){
+	function output( $ar = NULL ){
+
+		if(!$ar) $ar = $this->tree;
+
+		//echo "<pre>". htmlentities( print_r( $ar , 1 ) )."</pre>";
+		//exit;
+
+		foreach( $ar as $index => $tg ){
+
+			$tag = $tg['tag'];
+			$inner = $tg['inner'];
+			$open = $tg['open'];
+			$child = $tg['child'];
+
+			$this->output_html .= $tg['open']; // OPEN			
+			// Tem filho ?
+			if( $child ){		
+				
+				//echo "<pre>". htmlentities( print_r( $child , 1 ) )."</pre>";
+				//exit;
+
+				/*if( array_key_exists( $tag , $data) ){
+					$breadcrumb = $data[$tag];
+					//echo "<pre>". htmlentities( print_r( $tg['child'], 1 ) )."</pre>";
+				}*/
+
+				/* Abre a tag do pai */
+						
+				$this->output( $child );
+				return;
+				
+
+			} else {
+
+				$this->output_html .= $inner;
+				
+			}
+			$this->output_html .= "</".$tag.">"; // CLOSE
+		}
+
+		echo htmlentities( $this->reverse_hide_ignore_tag( $this->output_html ) );
+
+	}
+
+	function write_OLD( $data = NULL, $ar = NULL, $breadcrumb = '', $open_parent = '', $parent = ''){
 
 		if(!$ar) $ar = $this->tree;
 		//$this->output = '';
@@ -120,6 +166,7 @@ class tagx{
 		$this->output = $this->reverse_hide_ignore_tag( $this->output );
 
 	}
+
 	var $tag_hide ;
 	function hide_ignore_tag( $html ){
 		foreach( $this->ignore_tag as $i => $tgi ){
@@ -141,6 +188,32 @@ class tagx{
 	}
 	function open_close( $str, $tag ){
 		return (substr_count( $str, "<".$tag ) == substr_count( $str, "</".$tag.">" ));
+	}
+
+	function get_props( $open ){
+		$space_prop = preg_replace('/<\s*[\w]+(.*)?>/i','$1',$open);
+		if( $space_prop ){
+			//echo $space_prop."<BR>";
+			if( preg_match_all('/([\w]+)\s*\=\s*([\'\"]*[\w\s_-]*[\'\"]*)(\s|$)/i', $space_prop , $matches) ){
+
+				$prop = array();
+				//echo "<pre>".print_r($matches,1)."</pre>";
+				foreach( $matches[1] as $i => $m ){
+
+					$vlr = trim( preg_replace('/\'|\"/', '', $matches[2][$i]) );
+
+					if( $m == 'class' ){
+						$prop[$matches[1][$i]] = explode(' ',$vlr);	
+					} else {
+						$prop[$matches[1][$i]] = $vlr;
+					}
+					
+				}
+
+				return $prop;	
+			}
+			
+		}
 	}
 
 	var $no_tag_count = 1;
@@ -184,6 +257,7 @@ class tagx{
 
 						$_ar_[$index]['n'] = 1;
 						$_ar_[$index]['tag'] = $tag;
+						$_ar_[$index]['prop'] = $this->get_props( $before );
 						$_ar_[$index]['open'] = $before;
 						$_ar_[$index]['child'] = '';
 						$_ar_[$index]['inner'] = '';
@@ -226,38 +300,111 @@ class tagx{
 		if( count( $_ar_ ) ) return $_ar_;
 	}
 
-	function SeachArray( $needle, $haystack = NULL ){
+	var $breadcrumb = array();
+	var $breadcrumb_gone = array();
+	var $last_cel = NULL;
+
+	function SeachArray( $needle, $haystack = NULL, $level = 0 ){
+
+		$needle = trim( $needle );
 
 		if(!$haystack) $haystack = $this->tree;
 		//echo "<pre>". htmlentities( print_r( $haystack , 1 ) )."</pre>";
 		//exit;
 		$ret = '';
-		foreach( $haystack as $key => $value ) {
+		foreach( $haystack as $key => $value ) {			
+			//echo $key." - ".$value['tag']." - ".$level."<BR>";
+
+			if( $level == 0 ){
+				$this->breadcrumb = array_merge( $this->breadcrumb, $this->breadcrumb_gone );
+				$this->breadcrumb_gone = array(0);
+			} else {
+				$this->breadcrumb_gone[$level] = $key;
+			}
+
+			$this->last_cel = $value;
+
 	        if( $value['tag'] == $needle ){
+	        	
 	        	return $value['child'];
 	        	break;
-	        } else {	
+
+	        } else if( 	$needle[0] == '#' &&
+						$value['prop']['id'] == substr($needle, 1) ){
+
+				return $value['child'];
+	        	break;
+			} else {
+
 	        	if( is_array($value['child'])) {
-	        		$ret = $this->SeachArray($needle,$value['child']);
-	        	}
+	        		
+	        		$ret = $this->SeachArray($needle,$value['child'], ++$level );
+	        		if( !$ret ){
+	        			$level--;
+	        		}
+
+	        	} 
 	        } 
 	    }
+
 	    return $ret;
 	}
 
 	function Selector( $Selector ){
-		$Selector = preg_replace('/\s/i','',$Selector);
-		preg_match_all('/(^|>*)([\w]+)/i' , $Selector , $matches);
-		if( count($matches) ){
-			$ar = NULL;
-			foreach( $matches[2] as $tag ){
-				$ar = $this->SeachArray( $tag, $ar );
-			}
-			echo "<pre>". htmlentities( print_r( $ar ,1) )."</pre>";
+		
+		$Selector = preg_replace('/\s+/i',' ',$Selector);
+		if( preg_match_all('/([\W]*[\w-_\.]+)(\s*[\W]\s*|)/i' , $Selector , $matches) ){
 
-		}else{	
-			//
+			//echo "<pre>". print_r( $matches[1] , 1 ) ."</pre>";
+
+			$ar = NULL;
+			foreach( $matches[1] as $needle ){
+				
+				//echo $needle."<BR>";
+				$this->SeachArray( trim($needle), $this->last_cel );
+				//echo "<pre>". htmlentities( print_r( $this->last_cel , 1 ) )."</pre>";
+				//$this->last_cel = NULL;
+
+			}
+
+			$this->breadcrumb = array_merge( $this->breadcrumb, $this->breadcrumb_gone );	
+
+			//echo "<hr><pre>". htmlentities( print_r( $this->last_cel ,1) )."</pre><hr>";			
+			//echo "<pre>". htmlentities( print_r( $this->tree , 1 ) )."</pre>";
+
+			return $this;
 		}
+	}
+
+	function html( $text ){
+		$this->SetIndex( $text );
+		return $this;
+	}
+
+	function SetIndex( $text ){
+
+		$eval_str = array();
+		$n = 1;
+
+		foreach( $this->breadcrumb as $i ){
+
+			if( count( $this->breadcrumb ) == $n ){
+				$eval_str[] = '['.$i.'][\'inner\']';
+			} else{
+				$eval_str[] = '['.$i.'][\'child\']';
+			}
+
+			$n++;			
+		}
+
+		if( count($eval_str) ){
+			eval ( '$this->tree'.implode('',$eval_str)."= '".$text."';" );
+			$this->breadcrumb = '';
+			$this->breadcrumb = array();
+			$this->breadcrumb_gone = '';
+			$this->breadcrumb_gone = array();
+		}
+
 	}
 	
 }
--- teste2.php
+++ teste2.php
@@ -1,24 +1,14 @@
 <?php
 
-include_once( 'tagx.class.php' );
+require_once( 'class/tagx.class.php' );
+
 ob_start();
 ?>
 		<div>
-			<h4>{title}</h4>
-			<ul>
-			<category>
-				<li>{name}
-					<subategory>
-						<ul>
-							<li>{name}</li>
-						</ul>
-					</subategory>
-				</li>
-			</category>
-			</ul>
+			<h4 id='title' class='title outher'></h4>
+			<p></p>
 		</div>
 <?php
-
 $html = ob_get_contents();
 ob_clean();
 
@@ -31,24 +21,11 @@ ob_clean();
 </style>
 <?php
 
-$data = array(	'title'=> 'Produtos', 
-				'description' => 'sdf sdf sdgf sdg sdggkajhajsghgt khfdgshgd khsd gs',
-				'Nome'=>'Ulisses', 'Idade'=> '35 anos','Description'=>'oi',
-				'options' => array( array('value'=>'1', 'text' => 'Cliente'),
-									array('value'=>'2', 'text' => 'Empresa') ),
-				'category'	=> 	array( 	array( 'name' 	=> 'Pizza', 'subategory' => array( 	array('name'=>'Mussarela'),
-																							array('name'=>'Calabreza') ), 'tam' => "Grande" ),
-										array( 'name' 	=> 'Lanche' ) ) );
-
 $tagx = new tagx( $html );
+//$tagx->write($data);
+$tagx	->Selector( '#title')->html('Title Page')
+		->output();
 
-$tagx->write($data);
-$tagx->Selector( 'category > subategory' );
 
 
-/*echo $tagx->output;
-echo "<hr>";
-echo htmlentities( $tagx->output );
-$tagx->echo_ar();*/
-//
 ?>
